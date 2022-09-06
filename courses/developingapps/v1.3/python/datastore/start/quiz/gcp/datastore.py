8baefa17de78caa1e15789154381892855eef423
Edwards Moses <edwardsmoses3@gmail.com>
2022-09-06 14:42:27 +0000

    feat: python: save question to datastore

--- courses/developingapps/v1.3/python/datastore/start/quiz/gcp/datastore.py
+++ courses/developingapps/v1.3/python/datastore/start/quiz/gcp/datastore.py
@@ -12,34 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# TODO: Import the os module
+import os
 
-
-
-# END TODO
-
-# TODO: Get the GCLOUD_PROJECT environment variable
-
-
-
-# END TODO
+# Get the project id from the env variable
+project_id = os.getenv('GCLOUD_PROJECT')
 
 from flask import current_app
+from google.cloud import datastore
 
-# TODO: Import the datastore module from the google.cloud package
+# Create the Datastore client object using the project_id
+datastore_client = datastore.Client(project_id)
 
-
-
-# END TODO
-
-# TODO: Create a Cloud Datastore client object
-# The datastore client object requires the Project ID.
-# Pass through the Project ID you looked up from the
-# environment variable earlier
-
-
-
-# END TODO
 
 """
 Returns a list of question entities for a given quiz
@@ -51,41 +34,20 @@ Returns a list of question entities for a given quiz
 def list_entities(quiz='gcp', redact=True):
     return [{'quiz':'gcp', 'title':'Sample question', 'answer1': 'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D', 'correctAnswer': 1, 'author': 'Nigel'}]
 
-"""
-Create and persist and entity for each question
-The Datastore key is the equivalent of a primary key in a relational database.
-There are two main ways of writing a key:
-1. Specify the kind, and let Datastore generate a unique numeric id
-2. Specify the kind and a unique string id
-"""
+
 def save_question(question):
-# TODO: Create a key for a Datastore entity whose kind is Question
-    pass
+
+    # Creating the key for an entity in the Question Kind
+    key = datastore_client.key('Question')
     
+    # Creating the entity
+    q_entity = datastore.Entity(key=key)
 
-# END TODO
+    # Iterate over the form values supplied to the function
+    for q_prop, q_val in question.items():
+        q_entity[q_prop] = q_val
 
-# TODO: Create a Datastore entity object using the key
-
-    
-
-# END TODO
-
-# TODO: Iterate over the form values supplied to the function
-
-    
-
-# END TODO
-
-# TODO: Assign each key and value to the Datastore entity
-
-        
-
-# END TODO
+    # Save
+    datastore_client.put(q_entity)
 
 
-# TODO: Save the entity
-
-    
-
-# END TODO
