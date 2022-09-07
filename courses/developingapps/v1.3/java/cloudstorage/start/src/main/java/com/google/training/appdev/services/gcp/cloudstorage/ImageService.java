76fdbd6ffd5b7655c996007cf7a553cfad345cd2
Edwards Moses <edwardsmoses3@gmail.com>
2022-09-07 18:30:16 +0000

    feat: java: upload imagees to cloud storage

--- courses/developingapps/v1.3/java/cloudstorage/start/src/main/java/com/google/training/appdev/services/gcp/cloudstorage/ImageService.java
+++ courses/developingapps/v1.3/java/cloudstorage/start/src/main/java/com/google/training/appdev/services/gcp/cloudstorage/ImageService.java
@@ -15,12 +15,7 @@
  */
 package com.google.training.appdev.services.gcp.cloudstorage;
 
-// TODO: Write a star import for Cloud Storage
-
-
-
-// END TODO
-
+import com.google.cloud.storage.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -32,55 +27,23 @@ import java.util.Arrays;
 @Service
 public class ImageService {
 
-// TODO: Create the storage client
-// The StorageOptions class has a getDefaultInstance()
-// static method.
-// Use the getService() method to get the storage client
+    private static Storage storage = StorageOptions.getDefaultInstance().getService();
 
+    @Value("${google.storage.bucket}")
+    private String bucketName;
 
+    public String saveImage(MultipartFile file) throws IOException {
 
+        String fileName = System.nanoTime()+file.getOriginalFilename();
+        BlobInfo blobInfo =
+                storage.create(
+                        BlobInfo
+                                .newBuilder(bucketName, fileName)
+                                .setContentType(file.getContentType())
+                                .setAcl(new ArrayList<>(Arrays.asList(Acl.of(Acl.User.ofAllUsers(), Acl.Role.READER))))
+                                .build(),
+                        file.getInputStream());
 
-
-// END TODO
-
-// TODO: Get the name of the Cloud Storage bucket
-// Use a Spring @Value annotation to get the value
-// Get the value using ${google.storage.bucket}
-// This references the GCLOUD_BUCKET environment variable
-
-
-
-// END TODO
-
-
-    public String saveImage(MultipartFile file) 
-                                    throws IOException {
-    // The existing code in the method creates a unique name
-    // based on the file's original name. It has a 
-    // prefix generated using the current date and time.
-    // This should ensure that a new file upload won't 
-    // overwrite an existing object in the bucket
-    String fileName = System.nanoTime() +  
-                              file.getOriginalFilename();
-
-    // TODO: Create a new Cloud Storage object
-    // Use the BlobInfo class to represent this object
-    // Use the BlobInfo.Builder to customize the Blob
-    // Set the content type from the file
-    // Set the object ACL to Public Read
-
-
-
-    // END TODO
-
-    // TODO: Cloud Storage public URLs are in the form:
-    // https://storage.googleapis.com/[BUCKET]/[OBJECT]
-    // Use String concatentation to create return the URL
-
-    return "Replace with the Cloud Storage Public URL";
-
-    // END TODO
-
+        return "https://storage-download.googleapis.com/" + bucketName+ "/" +fileName;
     }
-
 }
