0f81c740e804317299318e7857c6e9502c460efb
Edwards Moses <edwardsmoses3@gmail.com>
2022-09-26 16:55:07 +0000

    feat:nodejs:cloud-functions: process cloud pub-sub trigger

--- courses/developingapps/v1.3/nodejs/cloudfunctions/start/function/index.js
+++ courses/developingapps/v1.3/nodejs/cloudfunctions/start/function/index.js
@@ -11,68 +11,38 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// TODO: Load the ./languageapi module
+const languageAPI = require('./languageapi');
+const feedbackStorage = require('./spanner');
 
 
-
-// END TODO
-
-// TODO: Load the ./spanner module
-
-
-
-// END TODO
 
 
 exports.subscribe = function subscribe(event) {
   // The Cloud Pub/Sub Message object.
-  // TODO: Decode the Cloud Pub/Sub message
-  // extracting the feedbackObject data
-  // The message received from Pub/Sub is base64 encoded, and 
-  // the data submitted by students is in a data property
   const pubsubMessage = Buffer.from(event.data, 'base64').toString();
+  let feedbackObject = JSON.parse(pubsubMessage);
+  console.log('Feedback object data before Language API:' + JSON.stringify(feedbackObject));
 
-
-  // END TODO
-
-  // TODO: Run Natural Language API sentiment analysis
-  // The analyze(...) method expects to be passed the 
-  // feedback text from the feedbackObject as an argument, 
-  // and returns a Promise.
-
-
-
+   return languageAPI.analyze(feedbackObject.feedback).then(score => {
     // TODO: Log the sentiment score
-
-
-
+  	console.log(`Score: ${score}`);
     // END TODO
-
     // TODO: Add new score property to feedbackObject
-
-
-
+  	feedbackObject.score = score;
     // END TODO
-
     // TODO: Pass feedback object to the next handler
-
-
-
+  	return feedbackObject;
     // END TODO
-
-  // TODO: insert record
-
-    // TODO: Log and return success
-
-
-
+  }).then(feedbackStorage.saveFeedback).then(() => {
+  	// TODO: Log and return success
+    console.log('feedback saved...');
+  	return 'success';
     // END TODO
+  })
+  .catch(console.error);
 
-  // END TODO
 
-  // TODO: Catch and Log error
 
-  // END TODO
 };
 
 
